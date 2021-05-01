beaf3b1b1d06b174e9907568498b4502a08e9e03
Sambit <sambitd@gmail.com>
2021-05-01 18:03:42 +1000

    Update libraries

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "h2o","recipies","readxl","tidyverse","tidyquant","lime","stringr","forcats")
+my_packages = c("shiny", "h2o","recipes","readxl","tidyverse","tidyquant","lime","stringr","forcats")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
